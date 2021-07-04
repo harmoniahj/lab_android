00e2e72558897550a0dbcf80c3aac4f675a6d96d
harmoniahj <harmoniahj@naver.com>
2021-07-04 13:32:38 +0900

    07월 04일 TerrGym80 로그인 추가

--- TerrGym80/app/src/main/java/com/example/terrgym80/MainActivity.java
+++ TerrGym80/app/src/main/java/com/example/terrgym80/MainActivity.java
@@ -1,17 +1,24 @@
 package com.example.terrgym80;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     FragmentManager fragmentManager = getSupportFragmentManager();
 
     @Override
@@ -43,11 +50,9 @@ public class MainActivity extends AppCompatActivity {
                         ,R.string.nav_close_drawer);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
-        fragmentManager.beginTransaction()
-                .replace(R.id.content_frame, LoginFragment.class, null)
-                .setReorderingAllowed(true)
-                .addToBackStack(null)
-                .commit();
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        //이벤트 소스와 이벤트 핸들러 연결
+        navigationView.setNavigationItemSelectedListener(this);
     }
     @Override
     protected void onStart() {
@@ -58,5 +63,47 @@ public class MainActivity extends AppCompatActivity {
         Log.i(this.getClass().getName(),"resList:"+v);
         Intent intent = new Intent(this,ResListActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        //어떤 메뉴아이템을 선택했니?
+        int id = item.getItemId();
+        Log.i(this.getClass().getName(),"선택한 메뉴의 아이디 값은 ===> "+id);
+        Fragment fragment = null;
+        Activity activity = null;
+        Intent intent = null;
+        switch (id){
+            case R.id.nav_pt:
+                fragment = new PtFragment();
+                break;
+            case R.id.nav_yoga:
+                fragment = new YogaFragment();
+                break;
+            case R.id.nav_spining:
+                fragment = new SpinningFragment();
+                break;
+            case R.id.nav_login:
+                intent = new Intent(this, LoginActivity.class);
+                break;
+            case R.id.nav_help:
+                intent = new Intent(this, HelpActivity.class);
+                break;
+            case R.id.nav_map:
+                intent = new Intent(this, MapActivity.class);
+                break;
+            default: break;
+        }
+        if(fragment !=null){//선택한 메뉴아이템이 프래그먼트야?
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.content_frame, fragment)
+                    .commit();
+        }else{//선택한 메뉴아이템이 액티비티인거야?
+            startActivity(intent);
+        }
+        //사용자가 옵션 중 하나를 선택하면 화면을 닫아요.
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
---
+++ TerrGym80/app/src/main/java/com/example/terrgym80/YogaFragment.java
@@ -0,0 +1,24 @@
+package com.example.terrgym80;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+/*
+BlankFragment - xml 필요
+
+ListFragment - layout  xml 없음
+ */
+
+public class YogaFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_yoga, container, false);
+    }
+}
